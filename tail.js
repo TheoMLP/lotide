b75473e1dc3803b2469310bcb1d5dd9e79301d55
TheoMLP <theomalaper.cognez@gmail.com>
2019-08-19 22:32:44 +0000

    tail function

---
+++ tail.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+assertEqual(tail(words).length, 2);
+
+
