e31f41e0eedcc6664c951aff62e3ce9b9ad137c2
TheoMLP <theomalaper.cognez@gmail.com>
2019-08-19 22:19:05 +0000

    head function

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+function head(array) {
+  return array[0]
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")
+assertEqual(head([1]), 1)
+assertEqual(head([]), undefined)
