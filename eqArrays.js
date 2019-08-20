0ea9ef94a959a7a452309e3aaf8c25b374d7657d
TheoMLP <theomalaper.cognez@gmail.com>
2019-08-20 22:01:46 +0000

    eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -6,22 +6,20 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
-    return false 
+    return false;
   }
   for (let value = 0; value < array1.length; value += 1) {
     if (array1[value] !== array2[value]) {
-      return false 
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
