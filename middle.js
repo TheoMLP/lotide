6287870cb4086c19400a21b708e9de73b2be1806
TheoMLP <theomalaper.cognez@gmail.com>
2019-08-20 22:45:14 +0000

    middle added

---
+++ middle.js
@@ -0,0 +1,32 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let value = 0; value < array1.length; value += 1) {
+    if (array1[value] !== array2[value]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+const middle = function(array) {
+  if (array.length <= 2) {
+    return []
+  } else if (array.length % 2 === 0) {
+    return [array[Math.ceil(array.length / 2) - 1], array[Math.ceil(array.length / 2)]];
+  } else {
+    return [array[Math.floor(array.length / 2)]];
+  }
+};
+
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
+
