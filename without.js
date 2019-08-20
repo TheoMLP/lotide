dcbb542a98f4b93c02a41094296bc8db112a2069
TheoMLP <theomalaper.cognez@gmail.com>
2019-08-20 23:08:23 +0000

    added without

---
+++ without.js
@@ -0,0 +1,37 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let value = 0; value < array1.length; value += 1) {
+    if (array1[value] !== array2[value]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+const without = function(array, value) {
+   newArray = []
+   array.forEach(function(element) {
+    if (value.includes(element)) {
+    } else { 
+      newArray.push(element)
+    }
+  })
+  return newArray
+}
+
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
+console.log(without([1, 2, 3], [1]));
+const words = ["hello", "world", "lighthouse"];
+console.log(without(["hello", "world", "lighthouse"], ["lighthouse"]));
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+assertArraysEqual(without([1, 2, 3], [3]), [1, 2])
