84e74276ebd20a9051c08b0514cfbb26dac7a557
TheoMLP <theomalaper.cognez@gmail.com>
2019-08-21 20:43:49 +0000

    finished assertObjectEqual

---
+++ assertObjectEqual.js
@@ -0,0 +1,41 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let value = 0; value < array1.length; value += 1) {
+    if (array1[value] !== array2[value]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (let key in object1) {
+  //for (let key in Object.keys(object1)) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      return eqArrays(object1[key], object2[key]);
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectEqual = function(object1, object2) {
+  const inspect = require('util').inspect;
+  if (eqObjects(object1, object2)) {
+    console.log(`✅  Assertion passed: ${inspect(object1)} === ${inspect(object2)}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${inspect(object1)} !== ${inspect(object2)}`);
+  }
+};
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectEqual(cd, dc);
+assertObjectEqual(cd, cd2);
