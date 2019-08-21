0a4e995350adfdd505c854967f671b91e723f1a7
TheoMLP <theomalaper.cognez@gmail.com>
2019-08-21 18:22:45 +0000

    countOnly.js added

---
+++ countOnly.js
@@ -0,0 +1,45 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+//given an array and object with condition 
+//return an object with count 
+//define an object 
+//object.keys the true one 
+//if the object.keys array includes elements of the array add it or add 1 to it 
+//return the object 
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {}
+
+  for (let item of allItems) {
+    //if (Object.keys(itemsToCount).includes(item))
+    if (itemsToCount[item])
+    !results[item] ? results[item] = 1 : results[item] += 1
+  }
+
+  console.log(results)
+  return results
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
