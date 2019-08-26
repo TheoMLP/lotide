0787b88ac828fb6aa4428eef1e6935f7f95d6153
TheoMLP <theomalaper.cognez@gmail.com>
2019-08-26 18:15:54 +0000

    refractor middle.js

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let value = 0; value < array1.length; value += 1) {
-    if (array1[value] !== array2[value]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅  Assertion passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
-  }
-};
-
 const middle = function(array) {
   if (array.length <= 2) {
     return [];
@@ -28,5 +8,5 @@ const middle = function(array) {
   }
 };
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+module.exports = middle; 
 
