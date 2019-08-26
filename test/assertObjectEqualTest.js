83c1e869258d491948ad9d9a81a8ff8ce7137c4d
TheoMLP <theomalaper.cognez@gmail.com>
2019-08-26 23:36:54 +0000

    refactored assertObjectEqualTest

--- test/assertObjectEqualTest.js
+++ test/assertObjectEqualTest.js
@@ -3,5 +3,6 @@ const assertObjectEqual = require("../assertObjectEqual")
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 const cd2 = { c: "1", d: ["2", 3, 4] };
+
 assertObjectEqual(cd, dc);
 assertObjectEqual(cd, cd2);
