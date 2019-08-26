edc2e664316684f43a6de4208ca9e03f1f1c64af
TheoMLP <theomalaper.cognez@gmail.com>
2019-08-26 22:27:12 +0000

    refactor without.js

--- without.js
+++ without.js
@@ -1,23 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let value = 0; value < array1.length; value += 1) {
-    if (array1[value] !== array2[value]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅  Assertion passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
-  }
-};
-
 const without = function(array, value) {
   let newArray = [];
   array.forEach(function(element) {
@@ -28,11 +8,5 @@ const without = function(array, value) {
   return newArray;
 };
 
-console.log(without(["1", "2", "3"], [1, 2, "3"]));
-console.log(without([1, 2, 3], [1]));
-const words = ["hello", "world", "lighthouse"];
-console.log(without(["hello", "world", "lighthouse"], ["lighthouse"]));
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-assertArraysEqual(without([1, 2, 3], [3]), [1, 2]);
-
 module.exports = without; 
+
