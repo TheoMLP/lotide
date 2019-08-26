0c90a2ebbcae74b77c079caefcf781f9dbd2ef99
TheoMLP <theomalaper.cognez@gmail.com>
2019-08-26 21:27:36 +0000

    refactor findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
 //find the key that contains the value
 //return the key
 //if no key is found return undefined
@@ -17,16 +9,5 @@ const findKeyByValue = function(object, value) {
     }
   }
 };
-
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
 
 module.exports = findKeyByValue;
---
+++ test/findKeyByValueTest.js
@@ -0,0 +1,23 @@
+const { assert } = require("chai");
+const findKeyByValue = require("../findKeyByValue");
+
+describe("#findKeyByValue", () => {
+  const bestTVShowsByGenre = {
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+  };
+
+  it("should return 'drama' when passed bestTVshowsByGenre and 'The Wire'", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  });
+  it("should return undefined when passed bestTVshowsByGenre and 'That '70s Show'", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+  });
+  it("should return 'sci_fi' when passed bestTVshowsByGenre and 'The Expanse'", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+  });
+  it("should return 'comedy' when passed bestTVshowsByGenre and 'Brooklyn Nine-Nine'", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+  });
+});
